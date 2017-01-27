c0b41b7d8a56873d49da981ce3abdcc4e00e9b40
vibecode <rumashkin@gmail.com>
2017-01-27 19:23:36 +0100

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+      <View>
+        <Text>
+          Test
+        </Text>
+      </View>
+  )
+};
+
+export default App;
