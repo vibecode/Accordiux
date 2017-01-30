f44b5f81d42a33f3691465ad1ade7131f4b199ae
vibecode <rumashkin@gmail.com>
2017-01-30 23:39:22 +0100

    expanding items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,9 +5,21 @@ import * as actions from '../actions';
 import { connect } from 'react-redux';
 
 class ListItem extends Component {
+  renderDescription() {
+    const {library, expanded} = this.props;
+
+    if (expanded) {
+      return (
+          <Text>
+            {library.description}
+          </Text>
+      );
+    }
+  }
+
   render() {
     const {titleStyle} = styles;
-    const { id, title } = this.props.library;
+    const {id, title} = this.props.library;
 
     return (
         <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
@@ -17,6 +29,7 @@ class ListItem extends Component {
                 {title}
               </Text>
             </CardSection>
+            {this.renderDescription()}
           </View>
         </TouchableWithoutFeedback>
     );
@@ -30,4 +43,10 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return {expanded};
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
